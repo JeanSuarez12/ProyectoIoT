2b639dfd43e165a656da515e11ea2d72a24f4270
FrediCaballero <frealcale@gmx.es>
2024-07-10 17:21:38 -0500

    update code

---
+++ Código/README.md
@@ -0,0 +1,5 @@
+Problemas por solucionar:
+- Añadir configuración MQTT
+- Velocidad de motor
+- La persiana solo sube al parecer, rompio la cortina :(
+- Validad la Logica para cuando se pase de manual a automático 
--- Código/sketch/sketch.ino
+++ Código/sketch/sketch.ino
@@ -1,19 +1,19 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
-#include <AccelStepper.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define DHTPIN 19
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Dirección I2C del LCD y tamaño (20x4)
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Dirección I2C del LCD y tamaño (16x2)
 
-// Driver de motor paso a paso A4988
-#define STEP_PIN 16
-#define DIR_PIN 17
-
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+// Pines del controlador L298N
+#define IN1_PIN 16
+#define IN2_PIN 17
+#define ENA_PIN 4
 
 // Pin analógico del LDR
 #define LDR_AO_PIN 34
@@ -24,10 +24,102 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 // Umbrales para temperatura, humedad y luz
 #define TEMP_THRESHOLD 25.0 // Ajustar según necesidad
-#define HUMIDITY_THRESHOLD 70.0 // Ajustar según necesidad
-#define LIGHT_THRESHOLD 2000 // Ajustar según necesidad (1024 es el máximo valor en una lectura analógica)
+#define HUMIDITY_THRESHOLD 80.0 // Ajustar según necesidad
+#define LIGHT_THRESHOLD 2000 // Ajustar según necesidad (4095 es el máximo valor en una lectura analógica)
 
+// Información de la red WiFi
+const char* ssid = "Alexander";
+const char* password = "12345678";
+
+// Información del servidor MQTT (Node-RED)
+const char* mqtt_server = "DIRECCION_DEL_SERVIDOR_MQTT";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Variables globales
 bool isCurtainOpen = false; // Bandera para rastrear el estado de la cortina
+bool manualMode = false; // Bandera para rastrear el modo manual/automático
+int motorSpeed = 2; // Velocidad actual del motor (255 es el 100%)
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  int retries = 0; // Contador de reintentos
+  while (WiFi.status() != WL_CONNECTED && retries < 20) {
+    delay(500);
+    Serial.print(".");
+    retries++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi conectado");
+    Serial.println("Dirección IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("");
+    Serial.println("No se pudo conectar al WiFi.");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incoming = "";
+  for (int i = 0; i < length; i++) {
+    incoming += (char)payload[i];
+  }
+  
+  Serial.print("Mensaje recibido de ");
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(incoming);
+  
+  // Cambiar entre modo manual y automático
+  if (String(topic) == "cortina/mode") {
+    if (incoming == "manual") {
+      manualMode = true;
+    } else if (incoming == "automatico") {
+      manualMode = false;
+    }
+  }
+  
+  // Control manual de la cortina
+  if (String(topic) == "cortina/control") {
+    if (manualMode) {
+      if (incoming == "abrir") {
+        abrirCortina();
+      } else if (incoming == "cerrar") {
+        cerrarCortina();
+      } else if (incoming == "detener") {
+        detenerCortina();
+      }
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conectar al servidor MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("conectado");
+      client.subscribe("cortina/mode");
+      client.subscribe("cortina/control");
+    } else {
+      Serial.print("falló, rc=");
+      Serial.print(client.state());
+      Serial.println(" intentando de nuevo en 5 segundos");
+      delay(5000);
+    }
+  }
+}
+
+
+
 
 void setup() {
   // Iniciar comunicación serial
@@ -43,9 +135,10 @@ void setup() {
   // Inicializar sensor DHT
   dht.begin();
 
-  // Inicializar motor paso a paso
-  stepper.setMaxSpeed(2000); // Aumentar la velocidad máxima del motor
-  stepper.setAcceleration(1000); // Aumentar la aceleración del motor
+  // Configurar pines del motor
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(ENA_PIN, OUTPUT);
 
   // Configurar pin analógico del LDR como entrada
   pinMode(LDR_AO_PIN, INPUT);
@@ -54,63 +147,105 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
+  // Conectar a WiFi
+  setup_wifi();
+
+  // Verificar conexión WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Error: No se pudo conectar al WiFi.");
+    // ESP.restart(); // Descomenta esta línea para reiniciar el ESP32 en caso de fallo
+  } else {
+    // Configurar cliente MQTT
+    //client.setServer(mqtt_server, 1883);
+    //client.setCallback(callback);
+  }
+
   // Mensaje de configuración completa
   Serial.println("Configuración completa.");
 }
 
 void loop() {
-  // Leer temperatura y humedad
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+  //if (!client.connected()) {
+  //  reconnect();
+  //}
+  //client.loop();
 
-  // Mostrar temperatura y humedad en el LCD
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temperature);
-  lcd.print(" C");
-  lcd.setCursor(0, 1);
-  lcd.print("Hum: ");
-  lcd.print(humidity);
-  lcd.print(" %");
+  if (!manualMode) {
+    // Leer temperatura y humedad
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
 
-  // Leer valor analógico del LDR
-  int ldrAnalogValue = analogRead(LDR_AO_PIN);
+    // Leer valor analógico del LDR
+    int ldrAnalogValue = analogRead(LDR_AO_PIN);
 
-  // Mostrar luminosidad en el LCD
-  lcd.setCursor(0, 2);
-  lcd.print("Luz: ");
-  if (ldrAnalogValue > LIGHT_THRESHOLD) {
-    lcd.print("Baja ");
-  } else {
-    lcd.print("Alta ");
-  }
+    // Leer distancia del HC-SR04 para detectar cortina completamente cerrada/abierta
+    long duration, distance;
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration / 2) / 29.1; // Convertir duración a distancia (cm)
 
-  // Controlar cortinas basado en el valor del LDR y las condiciones ambientales
-  if (ldrAnalogValue > LIGHT_THRESHOLD || temperature > TEMP_THRESHOLD || humidity > HUMIDITY_THRESHOLD) {
-    abrirCortina();
-  } else {
-    cerrarCortina();
-  }
 
-  // Leer distancia del HC-SR04 para detectar cortina completamente cerrada/abierta
-  long duration, distance;
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = (duration / 2) / 29.1; // Convertir duración a distancia (cm)
 
-  // Verificar si la cortina está completamente cerrada o abierta
-  if (distance > 200 && !isCurtainOpen) { // se colocaron 200 cm a modo de prueba
-    // La cortina está completamente abierta
-    isCurtainOpen = false;
-    detenerCortina();
-  } else if (distance < 40 && isCurtainOpen) { // se colocaron 40 cm a modo de prueba
-    // La cortina está completamente cerrada
-    isCurtainOpen = true;
-    detenerCortina();
+    // Mostrar valores en el LCD
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T:");
+    lcd.print(temperature, 1);
+    lcd.print(" C ");
+    lcd.print("H:");
+    lcd.print(humidity, 1);
+    lcd.print("%");
+    lcd.setCursor(0, 1);
+    lcd.print("Luz:");
+    lcd.print(ldrAnalogValue);
+    lcd.print(" lux");
+
+
+    /*
+    lcd.setCursor(0, 1);
+    lcd.print("Luz: ");
+    if (ldrAnalogValue < LIGHT_THRESHOLD) {
+      lcd.print("Baja ");
+    } else {
+      lcd.print("Alta ");
+    }
+    */
+
+
+    // Mostrar valores en el monitor serial
+    Serial.print("Distancia: ");
+    Serial.print(distance);
+    Serial.print(" cm, Temp: ");
+    Serial.print(temperature);
+    Serial.print(" C, Hum: ");
+    Serial.print(humidity);
+    Serial.print(" %, Luz: ");
+    Serial.println(ldrAnalogValue);
+
+
+
+    // Controlar cortinas basado en el valor del LDR y las condiciones ambientales
+    if (ldrAnalogValue > LIGHT_THRESHOLD && temperature > TEMP_THRESHOLD && humidity > HUMIDITY_THRESHOLD) {
+      abrirCortina();
+    } else {
+      cerrarCortina();
+    }
+
+
+    // Verificar si la cortina está completamente cerrada o abierta
+    if (distance > 32 && !isCurtainOpen) { // Se colocaron 32 cm a modo de prueba
+      // La cortina está completamente abierta
+      isCurtainOpen = true;
+      detenerCortina();
+    } else if (distance < 6 && isCurtainOpen) { // Se colocaron 6 cm a modo de prueba
+      // La cortina está completamente cerrada
+      isCurtainOpen = false;
+      detenerCortina();
+    }
   }
 
   // Retardo antes de la próxima iteración del bucle
@@ -120,31 +255,32 @@ void loop() {
 void abrirCortina() {
   if (!isCurtainOpen) {
     // Mostrar estado en el LCD
-    lcd.setCursor(0, 3);
-    lcd.print("Abriendo cortina    ");
+    lcd.setCursor(0, 1);
+    lcd.print("Abriendo cortina");
     // Girar motor en sentido horario para abrir la cortina
-    digitalWrite(DIR_PIN, HIGH);
-    stepper.move(10000); // Mover motor paso a paso
-    stepper.runSpeedToPosition(); // Ejecutar el movimiento
+    digitalWrite(IN1_PIN, HIGH);
+    digitalWrite(IN2_PIN, LOW);
+    analogWrite(ENA_PIN, motorSpeed); 
   }
 }
 
 void cerrarCortina() {
   if (isCurtainOpen) {
     // Mostrar estado en el LCD
-    lcd.setCursor(0, 3);
-    lcd.print("Cerrando cortina    ");
+    lcd.setCursor(0, 1);
     // Girar motor en sentido antihorario para cerrar la cortina
-    digitalWrite(DIR_PIN, LOW);
-    stepper.move(-10000); // Mover motor paso a paso
-    stepper.runSpeedToPosition(); // Ejecutar el movimiento
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, HIGH);
+    analogWrite(ENA_PIN, motorSpeed); 
   }
 }
 
 void detenerCortina() {
   // Detener el motor
-  stepper.stop();
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  analogWrite(ENA_PIN, 0); 
   // Mostrar estado en el LCD
-  lcd.setCursor(0, 3);
+  lcd.setCursor(0, 1);
   lcd.print("Cortina detenida    ");
 }
